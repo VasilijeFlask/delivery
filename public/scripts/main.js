9030a5389f160a5a7ba278c7ee6ce88856eea773
VasilijeFlask <vasilije.bjeloglav@gmail.com>
2023-11-27 19:26:33 +0100

    Item removing from order sucesuful

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -74,11 +74,12 @@ document.querySelectorAll('.add-item').forEach(button => {
         imageSrc: itemImageSrc
       };
       addItemToOrderDisplay(itemId);
+      button.disabled = true;
+
       updateItemCount()
 
     }
 
-    button.disabled = true;
   });
 });
 
@@ -109,48 +110,75 @@ function addItemToOrderDisplay(itemId) {
 
 
 document.querySelector('.order-details').addEventListener('click', event => {
+  // Find the closest parent .order-item element from the clicked target
+  const orderItem = event.target.closest('.order-item');
+  if (!orderItem) return; // Exit early if no order-item is found
 
-  if (event.target.closest('.plus') || event.target.closest('.minus')) {
-    const orderItem = event.target.closest('.order-item');
-    const itemId = orderItem.dataset.id;
+  // Retrieve the item ID
+  const itemId = orderItem.dataset.id;
+
+  // Check if the plus button was clicked
+  if (event.target.closest('.plus')) {
+    // Find the .count element and increment the count
     let countElement = orderItem.querySelector('.count');
     let count = parseInt(countElement.textContent, 10);
+    count += 1;
+    countElement.textContent = count.toString();
+    orderItems[itemId].count = count;
+  }
 
+  // Check if the minus button was clicked
+  else if (event.target.closest('.minus')) {
+    // Find the .count element and decrement the count, if greater than 1
+    let countElement = orderItem.querySelector('.count');
+    let count = parseInt(countElement.textContent, 10);
+    if (count > 1) {
+      count -= 1;
+      countElement.textContent = count.toString();
+      orderItems[itemId].count = count;
+    }
+  }
 
-    if (event.target.closest('.plus')) {
-      count += 1; 
-    } else if (event.target.closest('.minus') && count > 1) {
-      count -= 1; 
+  // Check if the trash button was clicked
+  else if (event.target.closest('.trash')) {
+    // Remove the item element from the DOM
+    orderItem.remove();
+    
+    // Remove the item from the orderItems object
+    delete orderItems[itemId];
+
+    const addButton = document.querySelector(`button[data-id="${itemId}"]`);
+    if (addButton) {
+      addButton.disabled = false;
     }
 
-    countElement.textContent = count.toString();
-
-    orderItems[itemId].count = count;
-
+    // Update the total number of items displayed
+    updateItemCount();
   }
 });
 
-
-// <<< DISPLAYING MENU ORDER >>> //
+// Function to update the total number of items
 function updateItemCount() {
   const itemCountElement = document.querySelector('.number-icon');
   let totalItems = 0;
   for (const id in orderItems) {
-    totalItems += orderItems[id].count; 
+    totalItems += orderItems[id].count;
   }
   itemCountElement.textContent = totalItems.toString(); 
-  console.log(totalItems); 
 }
-
-
-
-
-
 
 
 
 // <<< DISPLAYING TOTAL ORDER >>> //
 document.addEventListener('DOMContentLoaded', function() {
+
+  document.querySelectorAll('.grid-item').forEach(gridItem => {
+    const itemId = gridItem.dataset.id; // make sure each .grid-item has a data-id attribute
+    const addButton = gridItem.querySelector('.add-item');
+    if (addButton) {
+      addButton.setAttribute('data-id', itemId); // Set the data-id attribute on the "Dodaj" button
+    }
+  });
 
   function disableBodyScroll() {
     document.body.style.overflow = 'hidden';
