8035e1b46cff89c0139779d0e5110312a3d4610b
VasilijeFlask <vasilije.bjeloglav@gmail.com>
2024-01-09 13:34:17 +0100

    Login and register finished

--- public/scripts/login.js
+++ public/scripts/login.js
@@ -6,10 +6,18 @@ document.addEventListener('DOMContentLoaded', function() {
   var register = document.querySelector('.register-container')
   var registerBtn = document.querySelector('.ghost')
 
+  var loginBefore = document.querySelector('.login-before')
+  var loginAfter = document.querySelector('.login-after')
+  var loginBtn = document.querySelector('.login-btn')
 
   registerBtn.addEventListener('click', function() {
     overlay.style.display = 'none'
     register.style.display = 'block'
+  });
+
+  loginBtn.addEventListener('click', function() {
+    loginBefore.style.display = 'none'
+    loginAfter.style.display = 'block'
   });
 
 });
