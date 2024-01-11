256589520380253aeb6c28193875aefed6bcbce0
VasilijeFlask <vasilije.bjeloglav@gmail.com>
2024-01-11 19:29:25 +0100

    Frontend login finished until backend set up

--- public/scripts/login.js
+++ public/scripts/login.js
@@ -1,23 +1,46 @@
 // LOGIN
-console.log('function')
 document.addEventListener('DOMContentLoaded', function() {
-
-  var overlay = document.querySelector('.overlay')
-  var register = document.querySelector('.register-container')
-  var registerBtn = document.querySelector('.ghost')
 
   var loginBefore = document.querySelector('.login-before')
   var loginAfter = document.querySelector('.login-after')
   var loginBtn = document.querySelector('.login-btn')
-
-  registerBtn.addEventListener('click', function() {
-    overlay.style.display = 'none'
-    register.style.display = 'block'
-  });
+  var confirmLogin = document.querySelector('.confirm-login')
 
   loginBtn.addEventListener('click', function() {
     loginBefore.style.display = 'none'
     loginAfter.style.display = 'block'
   });
+
+  confirmLogin.addEventListener('click', function() {
+    event.preventDefault()
+    window.location.href = '/public/index.html'
+    console.log('here')
+  })
+
+
+
+  var overlay = document.querySelector('.overlay')
+  var register = document.querySelector('.register-container')
+  var registerBtnOne = document.querySelector('.register-one')
+
+  registerBtnOne.addEventListener('click', function() {
+    overlay.style.display = 'none'
+    register.style.display = 'block'
+  });
+
+  var payment = document.querySelector('.payment-container')
+  var registerBtnThree = document.querySelector('.register-three')
+  var registerBtnTwo = document.querySelector('.register-two')
+
+  registerBtnTwo.addEventListener('click', function() {
+    register.style.display = 'none'
+    payment.style.display = 'block'
+  });
+
+  
+
+  registerBtnThree.addEventListener('click', function() {
+    window.location.href = '/public/index.html'
+  })
 
 });
